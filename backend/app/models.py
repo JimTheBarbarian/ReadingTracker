0c90110b1391a7c1c0fba56e524e7f2aed55e08e
JimTheBarbarian <38097091+JimTheBarbarian@users.noreply.github.com>
2025-11-17 02:12:14 -0800

    built more of backend, created author and item tables

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,33 @@
 import uuid
 
 from pydantic import EmailStr
-from sqlalchemy import Field, Relationship
+from sqlalchemy import Field, Relationship, DelcarativeBase, Mapped, mapped_column, ForeignKey
+
+
+class Base(DelcarativeBase):
+    pass
+
+
+class AuthorBase(Base):
+    __tablename__ = "authors"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True, default=uuid.uuid4, index=True
+    )
+    name: Mapped[str] = mapped_column(nullable=False, index=True)
+
+    items: Mapped[list["ItemBase"]] = Relationship(back_populates="author")
+
+class ItemBase(Base):
+    __tablename__ = "items"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True, default=uuid.uuid4, index=True
+    )
+    name: Mapped[str] = mapped_column(nullable=False, index=True)
+    author: Mapped[str] = mapped_column(ForeignKey("authors.id"), nullable=False)
+    
+
+
+
+
